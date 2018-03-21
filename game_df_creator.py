52123da48aff2e6d87830d7bda14f0b4e1c487fe
Sean Costello <scostello28@gmail.com>
2018-03-21 07:41:50 -0600

    Add game_type column and reorganize repo

--- game_df_creator.py
+++ game_df_creator.py
@@ -1,8 +1,42 @@
 import pandas as pd
 import numpy as np
+from datetime import datetime
+from datetime import date
 import pickle
 
 team_names_sos_filepath = 'team_list/sos_team_list_2018_final.csv'
+
+seasons = [2014]   #[2014, 2015, 2016, 2017, 2018]
+
+season2013start = date(2012,4,1)
+season2013end = date(2013,3,18)
+tourney2013start = date(2013,3,19)
+tourney2013end = date(2013,4,8)
+
+season2014start = date(2013,4,9)
+season2014end = date(2014,3,17)
+tourney2014start = date(2014,3,18)
+tourney2014end = date(2014,4,7)
+
+season2015start = date(2014,4,8)
+season2015end = date(2015,3,16)
+tourney2015start = date(2015,3,17)
+tourney2015end = date(2015,4,6)
+
+season2016start = date(2015,4,7)
+season2016end = date(2016,3,14)
+tourney2016start = date(2016,3,15)
+tourney2016end = date(2016,4,4)
+
+season2017start = date(2016,4,5)
+season2017end = date(2017,3,13)
+tourney2017start = date(2017,3,14)
+tourney2017end = date(2017,4,3)
+
+season2018start = date(2017,4,4)
+season2018end = date(2018,3,12)
+tourney2018start = date(2018,3,13)
+tourney2018end = date(2018,4,2)
 
 def team_list(filepath):
     '''
@@ -38,84 +72,161 @@ def sos_dict(filepath):
         sos_dict[school] = sos
     return sos_dict
 
-
-def stat_transform(teams, season):
+def df_creator(teams, seasons):
     '''
     INPUTs:
-        teams = list of teams (formatted as in url)
+        team = team (formatted as in url)
         season = season year
 
     OUTPUT: DataFrame of all games
     '''
+
     games_df = pd.DataFrame()
 
-    for team in teams:
+    for season in seasons:
 
-        url = 'https://www.sports-reference.com/cbb/schools/{}/{}-gamelogs.html#sgl-basic::none'.format(team, season)
+        for team in teams:
 
-        '''Read team gamelog'''
-        df = pd.read_html(url)[0]
+            url = 'https://www.sports-reference.com/cbb/schools/{}/{}-gamelogs.html#sgl-basic::none'.format(team, season)
 
-        '''remove oppenent columns'''
-        df = df.iloc[:, 0:23]
+            '''Read team gamelog'''
+            df = pd.read_html(url)[0]
 
-        '''Remove divider rows'''
-        df = df.drop(df.index[[20,21]])
+            df = stat_transform(df, team)
 
-        '''Remove Double Column headers'''
-        dubcols = df.columns.tolist()
-        cols = [col[1] for col in dubcols]
-        df.columns = cols
-
-
-        '''Rename Columns'''
-        newcols = ['G', 'Date', 'Blank', 'Opp', 'W', 'Pts', 'PtsA', 'FG', 'FGA',
-                   'FG%', '3P', '3PA', '3P%', 'FT', 'FTA', 'FT%', 'ORB', 'RB',
-                   'AST', 'STL', 'BLK', 'TO', 'PF']
-        df.columns = newcols
-
-        '''reformat Opponent team name column strings'''
-        df['Opp'] = df['Opp'].map(teams_dict(team_names_sos_filepath))
-
-        '''Only take the first charcter in W field then map to 0's and 1's.
-        (Ties and overtime have excess characters)'''
-        df['W'] = df['W'].astype(str).str[0]
-        df['W'] = df['W'].map({'W': 1, 'L': 0})
-
-        '''Create win precentage and rolling average Features'''
-        df['Ws'] = df['W'].cumsum(axis=0)
-        df['Wp'] = df['Ws'].astype(int) / df['G'].astype(int)
-        df['ppg'] = df['Pts'].rolling(window=5,center=False).mean()
-        df['pApg'] = df['PtsA'].rolling(window=5,center=False).mean()
-        df['FGp'] = df['FG%'].rolling(window=5,center=False).mean()
-        df['3Pp'] = df['3P%'].rolling(window=5,center=False).mean()
-        df['FTp'] = df['FT%'].rolling(window=5,center=False).mean()
-        df['ORBpg'] = df['ORB'].rolling(window=5,center=False).mean()
-        df['RBpg'] = df['RB'].rolling(window=5,center=False).mean()
-        df['ASTpg'] = df['AST'].rolling(window=5,center=False).mean()
-        df['STLpg'] = df['STL'].rolling(window=5,center=False).mean()
-        df['BLKpg'] = df['BLK'].rolling(window=5,center=False).mean()
-        df['TOpg'] = df['TO'].rolling(window=5,center=False).mean()
-        df['PFpg'] = df['PF'].rolling(window=5,center=False).mean()
-
-        '''Remove columns after rolling ave calcs'''
-        df = df.drop(['G', 'Blank', 'Pts', 'PtsA', 'FG', 'FGA', 'FG%',
-                      '3P', '3PA', '3P%', 'FT', 'FTA', 'FT%', 'ORB', 'RB',
-                      'AST', 'STL', 'BLK', 'TO', 'PF'], axis=1)
-
-        '''Drop NaN rows before rolling averages can be calc'd'''
-        df = df.dropna()
-
-        '''Add Team Column'''
-        df['Tm'] = team
-
-        '''Add SOS columns'''
-        df['sos'] = df['Tm'].map(sos_dict(team_names_sos_filepath))
-
-        '''Add df to games_df'''
-        games_df = games_df.append(df, ignore_index=True)
+            '''Add df to games_df'''
+            games_df = games_df.append(df, ignore_index=True)
 
     return games_df
+
+
+def stat_transform(df, team):
+    '''
+    INPUTs:
+        df = dataframe created from html pull
+        team to add team column
+
+    OUTPUT: DataFrame of all games with clean and transformed data
+    '''
+
+    '''remove oppenent columns'''
+    df = df.iloc[:, 0:23]
+
+    '''Remove divider rows'''
+    df = df.drop(df.index[[20,21]])
+
+    '''Remove Double Column headers'''
+    dubcols = df.columns.tolist()
+    cols = [col[1] for col in dubcols]
+    df.columns = cols
+
+    '''Rename Columns'''
+    newcols = ['G', 'Date', 'Blank', 'Opp', 'W', 'Pts', 'PtsA', 'FG', 'FGA',
+               'FG%', '3P', '3PA', '3P%', 'FT', 'FTA', 'FT%', 'ORB', 'RB',
+               'AST', 'STL', 'BLK', 'TO', 'PF']
+    df.columns = newcols
+
+    '''reformat Opponent team name column strings'''
+    df['Opp'] = df['Opp'].map(teams_dict(team_names_sos_filepath))
+
+    '''Only take the first charcter in W field then map to 0's and 1's.
+    (Ties and overtime have excess characters)'''
+    df['W'] = df['W'].astype(str).str[0]
+    df['W'] = df['W'].map({'W': 1, 'L': 0})
+
+    '''Create win precentage and rolling average Features'''
+    df['Ws'] = df['W'].cumsum(axis=0)
+    df['Wp'] = df['Ws'].astype(int) / df['G'].astype(int)
+    df['ppg'] = df['Pts'].rolling(window=5,center=False).mean()
+    df['pApg'] = df['PtsA'].rolling(window=5,center=False).mean()
+    df['FGp'] = df['FG%'].rolling(window=5,center=False).mean()
+    df['3Pp'] = df['3P%'].rolling(window=5,center=False).mean()
+    df['FTp'] = df['FT%'].rolling(window=5,center=False).mean()
+    df['ORBpg'] = df['ORB'].rolling(window=5,center=False).mean()
+    df['RBpg'] = df['RB'].rolling(window=5,center=False).mean()
+    df['ASTpg'] = df['AST'].rolling(window=5,center=False).mean()
+    df['STLpg'] = df['STL'].rolling(window=5,center=False).mean()
+    df['BLKpg'] = df['BLK'].rolling(window=5,center=False).mean()
+    df['TOpg'] = df['TO'].rolling(window=5,center=False).mean()
+    df['PFpg'] = df['PF'].rolling(window=5,center=False).mean()
+
+    '''Remove columns after rolling ave calcs'''
+    df = df.drop(['G', 'Blank', 'Pts', 'PtsA', 'FG', 'FGA', 'FG%',
+                  '3P', '3PA', '3P%', 'FT', 'FTA', 'FT%', 'ORB', 'RB',
+                  'AST', 'STL', 'BLK', 'TO', 'PF'], axis=1)
+
+    '''Drop NaN rows before rolling averages can be calc'd'''
+    df = df.dropna()
+
+    '''Add Team Column'''
+    df['Tm'] = team
+
+    '''Add SOS columns'''
+    df['sos'] = df['Tm'].map(sos_dict(team_names_sos_filepath))
+
+    '''Add datetime formatted date without time of day (i.e. just the date)'''
+    df['just_date'] = pd.to_datetime(df['Date']).dt.date
+
+    return df
+
+def tourney_game_label(row):
+    '''
+    Create Column for tourney games
+    '''
+
+    if row['just_date'] >= tourney2013start and row['just_date'] <= tourney2013end:
+
+        row['GameType'] = 'tourney2013'
+
+    elif row['just_date'] >= season2013start and row['just_date'] <= season2013end:
+
+        row['GameType'] = 'season2013'
+
+    elif row['just_date'] >= tourney2014start and row['just_date'] <= tourney2014end:
+
+        row['GameType'] = 'tourney2014'
+
+    elif row['just_date'] >= season2014start and row['just_date'] <= season2014end:
+
+        row['GameType'] = 'season2014'
+
+    elif row['just_date'] >= tourney2015start and row['just_date'] <= tourney2015end:
+
+        row['GameType'] = 'tourney2015'
+
+    elif row['just_date'] >= season2015start and row['just_date'] <= season2015end:
+
+        row['GameType'] = 'season2015'
+
+    elif row['just_date'] >= tourney2016start and row['just_date'] <= tourney2016end:
+
+        row['GameType'] = 'tourney2016'
+
+    elif row['just_date'] >= season2016start and row['just_date'] <= season2016end:
+
+        row['GameType'] = 'season2016'
+
+    elif row['just_date'] >= tourney2017start and row['just_date'] <= tourney2017end:
+
+        row['GameType'] = 'tourney2017'
+
+    elif row['just_date'] >= season2017start and row['just_date'] <= season2017end:
+
+        row['GameType'] = 'season2017'
+
+    elif row['just_date'] >= tourney2018start and row['just_date'] <= tourney2018end:
+
+        row['GameType'] = 'tourney2018'
+
+    elif row['just_date'] >= season2018start and row['just_date'] <= season2018end:
+
+        row['GameType'] = 'season2018'
+
+    else:
+
+        row['GameType'] = 'season'
+
+    return row
 
 def gen_unique_id(row):
     '''
@@ -164,19 +275,18 @@ def save_to_pkl(df, year):
     df.to_pickle('games_{}.pkl'.format(year))
 
 if __name__ == '__main__':
-    filepath = 'team_list/sos_team_list_2018_final.csv'
-    years = [2013, 2014, 2015, 2016, 2017]
-    for year in years:
-
-    teams = team_list(filepath)
-    games = stat_transform(teams, year)
-    games = games.apply(gen_unique_id, axis=1)
-    games = everybody_merge(games)
-    save_to_pkl(games, year)
-
-    # filepath = 'team_list/sos_team_list_2018_final.csv'
-    # year = 2017
+    # # filepath = 'team_list/sos_team_list_2018_final.csv'
     # teams = team_list(filepath)
-    # games = df_creator(teams, year)
+    # games = df_creator(teams, seasons)
+    # games = games.apply(tourney_game_label, axis=1)
     # games = games.apply(gen_unique_id, axis=1)
+    # games = everybody_merge(games)
     # save_to_pkl(games, year)
+
+
+    teams = team_list(team_names_sos_filepath)
+    games = df_creator(teams, seasons)
+    games = games.apply(tourney_game_label, axis=1)
+    games = games.apply(gen_unique_id, axis=1)
+    # games = everybody_merge(games)
+    save_to_pkl(games, year='all_years')
